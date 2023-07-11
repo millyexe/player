76e221b1b56f067df56588feb124ba01c3fa80e0
millyexe <milenalobko18@outlook.com>
2023-07-10 20:20:16 -0400

    wave player

--- app.js
+++ app.js
@@ -120,15 +120,18 @@ Array.from(document.getElementsByClassName("songItem")).forEach(
 );
 
 let masterPlay = document.getElementById("masterPlay");
+let wave = document.getElementsByClassName("wave")[0];
 
 masterPlay.addEventListener("click", () => {
   if (music.paused || music.currentTime <= 0) {
     music.play();
     masterPlay.classList.remove("bi-play-fill");
     masterPlay.classList.add("bi-pause-fill");
+    wave.classList.add("active2");
   } else {
     music.pause();
     masterPlay.classList.add("bi-play-fill");
     masterPlay.classList.remove("bi-pause-fill");
+    wave.classList.remove("active2");
   }
 });
