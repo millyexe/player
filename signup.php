f2fc5dd030068284a543d27b6070428996f446b0
millyexe <milenalobko18@outlook.com>
2023-07-28 22:36:08 -0400

    wwww

--- signup.php
+++ signup.php
@@ -13,13 +13,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 if (!empty($user_name) && !empty($user_email) && !empty($user_password) && !is_numeric($user_name)){
  if ($user_password === $user_re_password) {
- $query = "insert into user (name, email, password) VALUES ("$user_name", "$user_email", "$user_password", "$user_re_password")";
+ $query = "insert into user (name, email, password) VALUES ("$user_name", "$user_email", "$user_password")";
  mysqli_query($con, $query);
  header("Location: login.php");
 } else {
 $msg = "Password Not Match";
  }
-  
  }
 } 
 ?> 
